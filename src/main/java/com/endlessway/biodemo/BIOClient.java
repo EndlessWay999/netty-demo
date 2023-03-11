528e951abcb9f9a6441511d038659cd9d6a3fc30
endlessway <endlessway@163.com>
2023-03-11 20:46:12 +0800

    bio基础

---
+++ src/main/java/com/endlessway/biodemo/BIOClient.java
@@ -0,0 +1,24 @@
+package com.endlessway.biodemo;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.net.Socket;
+
+public class BIOClient {
+    public static void main(String[] args) {
+        try {
+            //1.创建Socket对象请求服务端的连接
+            Socket socket = new Socket("127.0.0.1",6666);
+            //2.从Socket对象中获取一个字节输出流
+            OutputStream os = socket.getOutputStream();
+            //3.把字节输出流包装成一个打印流
+            PrintStream ps = new PrintStream(os);
+            //ps.print("hello World! 服务端，你好");
+            ps.println("hello World! 服务端，你好");
+            ps.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
